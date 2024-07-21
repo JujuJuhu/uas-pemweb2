5657d87d6da9976a170ea28184f24cfcddbb6cba
Jovita Kusuma <130336044+JujuJuhu@users.noreply.github.com>
2024-07-21 11:13:40 +0700

    Add files via upload

---
+++ application/views/admin/Developer.php
@@ -0,0 +1,12 @@
+<!--
+@Project: SkillSphere
+@Programmer: Kelompok 2
+@Website: https://linktr.ee/jovitakhu
+@Email : jovita.khu@gmail.com
+
+@About-Learnify :
+Web Edukasi Open Source yang dibuat oleh Kelompok 2.
+SkillSphere adalah Web edukasi yang dilengkapi video, materi dan sistem ujian
+yang tersedia secara gratis. SkillSphere dibuat ditujukan agar para siswa dan
+guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
+-->
---
+++ application/views/index.php
@@ -0,0 +1,266 @@
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+	<div class="banner_inner d-flex align-items-center">
+		<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+		</div>
+		<div class="container">
+			<div class="banner_content text-center">
+				<h3 data-aos="fade-up" data-aos-duration="1600">SkillSphere: <br /> Kuasai Skill Baru Kapanpun dan Dimanapun</h3>
+				<p data-aos="fade-up" data-aos-duration="1900"> Project Website LMS untuk UAS Mata Kuliah Pemrograman Website 2 - Universitas Siber Asia
+					Empowering Your Future, One Skill at a Time</p>
+				<a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?= base_url('user/registration') ?>#registration">Join Now!<span class="lnr lnr-arrow-right"></span></a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ================End Home Banner Area ================= -->
+
+<!--================Finance Area =================-->
+<section class="finance_area">
+	<div class="container">
+		<div class="finance_inner row">
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1600" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-users"></i>
+						</div>
+						<div class="media-body">
+							<h5>Admin, User & Tutor Data Management</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1800" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-file-empty"></i>
+						</div>
+						<div class="media-body">
+							<h5>Dokumentasi lengkap & Jelas</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div data-aos="fade-right" data-aos-duration="2000" class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-camera-video"></i>
+						</div>
+						<div class="media-body">
+							<h5>E-Learning Berbasis Video dan Disqus</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div data-aos="fade-right" data-aos-duration="2200" class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-tag"></i>
+						</div>
+						<div class="media-body">
+							<h5>Gratis dengan Lisensi MIT Open-Source</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Finance Area =================-->
+
+<!--================ Illustrations Area =================-->
+<section class="learnify-for-indonesia p_20">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 mx-auto">
+				<img data-aos="fade-up" data-aos-duration="1800" src="<?= base_url('assets/') ?>img/illustrations/index-study.svg" alt="" srcset="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-7 mx-auto">
+				<div class="main_title">
+					<h2 data-aos="fade-up" data-aos-duration="2000">SkillSphere Dibuat Untuk Meningkatkan Kualitas Pembelajaran Di Indonesia</h2>
+					<p data-aos="fade-up" data-aos-duration="2200">SkillSphere adalah platform pembelajaran daring yang dirancang untuk membantu Anda mengembangkan keterampilan baru dan meningkatkan pengetahuan di berbagai bidang. Dengan beragam kursus yang tersedia, mulai dari teknologi, bisnis, seni, hingga keterampilan pribadi, SkillSphere memberikan akses kepada materi pembelajaran berkualitas tinggi yang dapat diakses kapan saja dan di mana saja. </p>
+					<a href="https://github.com/syauqi/learnify"><button data-aos="fade-up" data-aos-duration="2400" class="bubbly-button">Download SkillSphere <span class="lnr lnr-arrow-right"></span></button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Illustrations Area =================-->
+
+<!--================Courses Area =================-->
+<section class="courses_area p_40">
+	<div class="container">
+		<div class="main_title">
+			<h2 data-aos="fade-up" data-aos-duration="1600">Pelajaran Yang Tersedia di SkillSphere</h2>
+			<p data-aos="fade-up" data-aos-duration="1800">Melalui pendekatan pembelajaran yang interaktif dan fleksibel, SkillSphere tidak hanya memudahkan proses belajar, tetapi juga memotivasi pengguna untuk terus berkembang dan mencapai tujuan mereka. Bergabunglah dengan SkillSphere dan mulai perjalanan Anda menuju masa depan yang lebih cerah dan penuh keterampilan!</p>
+		</div>
+		<div class="row courses_inner">
+			<div class="col-lg-9">
+				<div class="grid_inner">
+					<div class="grid_item wd55">
+						<div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-1.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Gratis</a>
+								<a href="javaScript:void(0);">
+									<h4>Kelas A</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i>54</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Tutor 1</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd44">
+						<div class="courses_item" data-aos="fade-down" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-2.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Gratis</a>
+								<a href="javaScript:void(0);">
+									<h4>Kelas B</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Tutor B</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd44">
+						<div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-4.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Gratis</a>
+								<a href="javaScript:void(0);">
+									<h4>Kelas C</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 63</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Tutor C</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="grid_item wd55">
+						<div class="courses_item" data-aos="fade-up" data-aos-duration="1800">
+							<img src="<?= base_url('assets/') ?>img/courses/course-5.jpg" alt="">
+							<div class="hover_text">
+								<a class="cat" href="#">Gratis</a>
+								<a href="javaScript:void(0);">
+									<h4>Kelas D</h4>
+								</a>
+								<ul class="list">
+									<li><a href="#"><i class="lnr lnr-users"></i> 24</a></li>
+									<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+									<li><a href="#"><i class="lnr lnr-user"></i> Tutor D</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="course_item" data-aos="fade-left" data-aos-duration="1800">
+					<img src="<?= base_url('assets/') ?>img/courses/course-3.jpg" alt="">
+					<div class="hover_text">
+						<a class="cat" href="#">Gratis</a>
+						<a href="javaScript:void(0);">
+							<h4>Kelas E</h4>
+						</a>
+						<ul class="list">
+							<li><a href="#"><i class="lnr lnr-users"></i> 35</a></li>
+							<li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+							<li><a href="#"><i class="lnr lnr-user"></i> Tutor E</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Courses Area =================-->
+
+<!--================Team Area =================-->
+<section class="team_area p_20">
+	<div class="container">
+		<div class="main_title">
+			<h2 data-aos="fade-up" data-aos-duration="1800">Testimonial SkillSphere</h2>
+			<p data-aos="fade-up" data-aos-duration="2000">"Your Journey to Expertise Starts Here"</p>
+		</div>
+		<section class="testimonials_area p_20">
+			<div class="container">
+				<div class="testi_slider owl-carousel">
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-3.png" alt="">
+							<h4>Chitato</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>SkillSphere adalah platform e-learning yang luar biasa, menawarkan berbagai kursus yang dirancang untuk membantu pengguna mengembangkan keterampilan baru dan meningkatkan pengetahuan mereka.</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-2.png" alt="">
+							<h4>Lays</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>Antarmuka yang intuitif dan konten yang berkualitas tinggi membuat pembelajaran menjadi lebih mudah dan menyenangkan. Sangat direkomendasikan untuk siapa saja yang ingin meningkatkan keterampilan profesional maupun pribadi!</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testi_item">
+							<img src="<?= base_url('assets/') ?>img/testimonials/testi-1.png" alt="">
+							<h4>Eureka</h4>
+							<ul class="list">
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+								<li><a href="#"><i class="fa fa-star"></i></a></li>
+							</ul>
+							<p>Dengan fleksibilitas untuk belajar kapan saja dan di mana saja, SkillSphere benar-benar memberdayakan penggunanya untuk mencapai potensi penuh mereka. 
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
+<!--================End Team Area =================-->
+
+<!--================Impress Area =================-->
+<section class="impress_area p_120">
+	<div class="container">
+		<div class="impress_inner text-center">
+			<h2 data-aos="fade-up" data-aos-duration="1800">LOGIN SEBAGAI TUTOR DAN UPLOAD MATERI & VIDEO SEKARANG</h2>
+			<p data-aos="fade-up" data-aos-duration="2000">Welcome to the SkillSphere Tutor Login Page! As a valued member of our teaching community, you play a crucial role in shaping the future of our learners. Log in to access your dashboard, manage your courses, interact with students, and track their progress. Thank you for your dedication to providing high-quality education and empowering learners to achieve their goals. If you need any assistance, our support team is here to help. Let's continue to inspire and educate together!
+			</p>
+			<a data-aos="fade-up" data-aos-duration="2200" class="main_btn" href="<?= base_url('welcome/guru') ?>">Login Sebagai Tutor <span class="lnr lnr-arrow-right text-black"></span></a>
+		</div>
+	</div>
+</section>
+<!--================End Impress Area =================-->
